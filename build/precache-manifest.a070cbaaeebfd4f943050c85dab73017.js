5ed6ff26fddad88d45bd9858725f3563d39d2717
ngecu <devngecu@gmail.com>
2023-06-14 19:01:46 +0300

    added build folder

---
+++ build/precache-manifest.a070cbaaeebfd4f943050c85dab73017.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4359225a6af8dd1709b7c7d58ba9dfa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c898f478c784c37b55a2",
+    "url": "/static/css/main.3322ca4d.chunk.css"
+  },
+  {
+    "revision": "9c98c80914a7a6fe0fa9",
+    "url": "/static/js/2.3c8258dc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3c8258dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c898f478c784c37b55a2",
+    "url": "/static/js/main.ca1df468.chunk.js"
+  },
+  {
+    "revision": "329102c932a6288a601d",
+    "url": "/static/js/runtime-main.c0c6e36b.js"
+  }
+]);
